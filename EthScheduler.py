2ab04bfcdb2ba8f25afac045d4d0299a445f6d9c
Bradley <bjschwa2@gmail.com>
2017-11-07 12:21:40 -0500

    adding and removing items save items to disk

--- EthScheduler.py
+++ EthScheduler.py
@@ -5,8 +5,11 @@ import EthSchedulerGUI # This file holds MainWindow and all design related thing
 import EthSchedulerDialog
 import subprocess
 import os
+import schedule
+import time
 
-DEFAULT_MINING_ADDRESS = "A688479f3C579Fb7F57A4833dA48e393de3F7a98"
+DEFAULT_MINING_ADDRESS = "0x41B145f770e5FCFd691aCFD9E94aaE19817d52b9"
+DEFAULT_CONFIG_LOCATION = "/home/bradley/.eth/"
 
 class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
 
@@ -17,6 +20,7 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
                             # It sets up layout and widgets that are defined
 
         self.currentWorkers = 0
+        self.workers = {}
 
         # setup signals and slots
         self.actionAdd_Worker.triggered.connect(self.addWorker)
@@ -24,16 +28,50 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
         self.start_worker_pushButton.clicked.connect(self.startWorker)
         self.actionQuit.triggered.connect(self.close)
 
+        # fill table from file on disk
+        self.readWorkerFile()
+
+        for key, value in sorted(self.workers.items()):
+            self.worker_tableWidget.insertRow(self.currentWorkers)
+            self.worker_tableWidget.setItem(self.currentWorkers, 0, QtGui.QTableWidgetItem(value['name']))
+            self.worker_tableWidget.setItem(self.currentWorkers, 1, QtGui.QTableWidgetItem(value['ip']))
+            self.worker_tableWidget.setItem(self.currentWorkers, 2, QtGui.QTableWidgetItem("IDLE"))
+            self.worker_tableWidget.setItem(self.currentWorkers, 3, QtGui.QTableWidgetItem(value['startTime']))
+            self.worker_tableWidget.setItem(self.currentWorkers, 4, QtGui.QTableWidgetItem(value['address']))
+            self.currentWorkers =self.currentWorkers+1
+        # start schedules for each item
+
+
+    def updateWorkerFile(self):
+        '''
+        updates a file on disk containing workers
+        '''
+        target = open(DEFAULT_CONFIG_LOCATION+'workers.txt', 'w')
+        target.write(str(self.workers))
+
+
+    def readWorkerFile(self):
+        '''
+        read the worker file into memory
+        '''
+        if os.path.isfile(DEFAULT_CONFIG_LOCATION+'workers.txt'):
+            with open(DEFAULT_CONFIG_LOCATION+'workers.txt', 'r') as f:
+                s = f.read()
+                self.workers = eval(s)
+
 
     def addWorker(self):
         '''
         add a worker to the interface
         '''
-        ip, name, startTime, endTime, address, ok = EthSchedulerDialog.AddWorkerDialog.addWorker()
+        worker = {}
+
+
+        username, ip, name, startTime, endTime, address, ok = EthSchedulerDialog.AddWorkerDialog.addWorker()
         if ok != QtGui.QDialog.Accepted:
             return
 
-
+        # add worker to the next line in the table
         self.worker_tableWidget.insertRow(self.currentWorkers)
         self.worker_tableWidget.setItem(self.currentWorkers, 0, QtGui.QTableWidgetItem(name))
         self.worker_tableWidget.setItem(self.currentWorkers, 1, QtGui.QTableWidgetItem(ip))
@@ -41,12 +79,33 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
         self.worker_tableWidget.setItem(self.currentWorkers, 3, QtGui.QTableWidgetItem(startTime + "-" + endTime))
         self.worker_tableWidget.setItem(self.currentWorkers, 4, QtGui.QTableWidgetItem(address))
 
+        worker['username'] = str(username)
+        worker['name'] = str(name)
+        worker['ip'] = str(ip)
+        worker['startTime'] = str(startTime)
+        worker['endTime'] = str(endTime)
+        worker['address'] = str(address)
 
+        self.workers[str(name)] = worker
+        self.currentWorkers =self.currentWorkers+1
+
+        #append to the file on disk
+        self.updateWorkerFile();
 
     def deleteWorker(self):
         '''
+        remove a worker from the interface
         '''
-        return
+        currentRow = self.worker_tableWidget.currentRow()
+        currentName = self.worker_tableWidget.item(currentRow, 0).text()
+
+        for key, item in  self.workers.items():
+            if key == currentName:
+                del self.workers[key]
+
+        self.updateWorkerFile();
+
+        self.worker_tableWidget.removeRow(currentRow)
 
 
     def startWorker(self):
@@ -55,12 +114,9 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
         currentRow = self.worker_tableWidget.currentRow()
         currentIp = self.worker_tableWidget.item(currentRow, 1).text()
 
-        if self. start_worker_pushButton.isChecked():
-            
-
+        if self.start_worker_pushButton.isChecked():
             addressName = self.worker_tableWidget.item(currentRow, 4).text()+"."+self.worker_tableWidget.item(currentRow, 0).text()
-
-            cmd = ["./ethminer"]
+            cmd = ["~/.eth/ethminer"]
             cmd.append('--farm-recheck')
             cmd.append('2000')
             cmd.append('-G')
@@ -69,17 +125,17 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
             cmd.append('-FS')
             cmd.append('us1.ethermine.org:14444')
             cmd.append('-O')
-            cmd.append( addressName)
+            cmd.append( str(addressName))
 
             print(cmd)
+            self.runRemoteProcess(str(currentIp), ' '.join(cmd))
             self.start_worker_pushButton.setText('Stop Worker')
-            self.runRemoteProcess(currentIp, ' '.join(cmd))
             self.worker_tableWidget.setItem(currentRow, 2, QtGui.QTableWidgetItem('RUNNING'))
         else:
             cmd = ["pkill"]
             cmd.append('-15')
             cmd.append('ethminer')
-            self.runRemoteProcess(currentIp,' '.join(cmd))
+            self.runRemoteProcess(str(currentIp),' '.join(cmd))
             self.start_worker_pushButton.setText('Start Worker')
             self.worker_tableWidget.setItem(currentRow, 2, QtGui.QTableWidgetItem('IDLE'))
 
@@ -89,7 +145,6 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
         runs Acos on all the tegras
         '''
         runRemoteProcess = subprocess.Popen(["ssh", "mosaic@%s"% ip, cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        runRemoteProcess.wait()
 
     def closeEvent(self, event):
         '''
@@ -108,6 +163,7 @@ class EthScheduler(QtGui.QMainWindow, EthSchedulerGUI.Ui_EthScheduler, ):
         '''
         shuts down the GUI
         '''
+        # stop all workers?
 
         self.close()
 
--- EthSchedulerDialog.py
+++ EthSchedulerDialog.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtGui # Import the PyQt4 
 from PyQt4 import QtCore # Import the PyQt4 
 
-DEFAULT_MINING_ADDRESS = "A688479f3C579Fb7F57A4833dA48e393de3F7a98"
+DEFAULT_MINING_ADDRESS = "0x41B145f770e5FCFd691aCFD9E94aaE19817d52b9"
 
 class AddWorkerDialog(QtGui.QDialog):
     def __init__(self, parent = None):
@@ -9,17 +9,21 @@ class AddWorkerDialog(QtGui.QDialog):
 
         layout = QtGui.QFormLayout(self)
 
+
         # nice widget for editing the date
+        self.username = QtGui.QLineEdit("mosaic")
+        layout.addRow("Username: ", self.username)
+
         self.ip = QtGui.QLineEdit("127.0.0.1")
         layout.addRow("IP: ", self.ip)
 
         self.name = QtGui.QLineEdit("Worker1")
         layout.addRow("Name: ", self.name)
 
-        self.startTime = QtGui.QLineEdit("16:00:00")
+        self.startTime = QtGui.QLineEdit("16:00")
         layout.addRow("Start Time: ", self.startTime)
 
-        self.endTime = QtGui.QLineEdit("6:00:00")
+        self.endTime = QtGui.QLineEdit("6:00")
         layout.addRow("End Time: ", self.endTime)
 
         self.address = QtGui.QLineEdit(DEFAULT_MINING_ADDRESS)
@@ -33,6 +37,10 @@ class AddWorkerDialog(QtGui.QDialog):
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
         layout.addRow(buttons)
+
+        # get current date and time from the dialog
+    def getUsername(self):
+        return self.username.text()
 
     # get current date and time from the dialog
     def getIp(self):
@@ -55,9 +63,10 @@ class AddWorkerDialog(QtGui.QDialog):
     def addWorker(parent = None):
         dialog = AddWorkerDialog(parent)
         result = dialog.exec_()
+        username = dialog.getUsername()
         ip = dialog.getIp()
         name = dialog.getName()
         startTime = dialog.getStartTime()
         endTime = dialog.getEndTime()
         address = dialog.getAddress()
-        return (ip, name, startTime, endTime, address , result == QtGui.QDialog.Accepted)
+        return (username, ip, name, startTime, endTime, address , result == QtGui.QDialog.Accepted)
